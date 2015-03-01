2477d04688cc6647e6de8a2814cc5ffd39f437c9
ajb413 <bavosa13@yahoo.com>
2015-03-01 14:01:56 -0500

    second

---
+++ rave.js
@@ -0,0 +1,34 @@
+$(function()
+{
+	var bd = $('body');
+
+	var classname = "invertimg";
+
+	var i = 0;
+	var next = i + 1;
+	setInterval(function()
+	{
+		
+		if(i === 9)
+			next = 0;
+		else
+			next = i + 1;
+		bd.switchClass(classname + i, classname + (next));
+		i++;
+		if(i === 10)
+		{
+			i = 0;
+		}
+
+	}, 100);
+
+});
+
+var p = chrome.extension.getURL('ss.ogg');
+
+$("<audio></audio>").attr({
+	
+    'src': p,
+    'autoplay':'autoplay',
+    'type' :'audio/mpeg'
+}).appendTo("body");
